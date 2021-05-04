aa3763558a71408b142a709468c87460ac4b3169
dunyahasan <78453394+dunyahasan@users.noreply.github.com>
2021-05-03 23:47:04 -0400

    Update homework.py

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,8 @@
 def spiralize(size, n=1):
-    """ lorum ipsum lorum ipsume"""
+    #for loop where 501 by 501 dimensions have a spiral summed 
+    
+    # sum the spiral where 501 * 501 == 251,001
+    
+    # n== "anything"
     return_value = n
     return return_value
